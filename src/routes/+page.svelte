13618e057401014c8bc61dab15c6ef0d26393b8e
poudelasbeen <asbeenpoudel613@gmail.com>
2023-10-08 22:25:29 +0545

    first Commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,9 @@
+<script>
+	import Footer from "../components/Footer.svelte";
+	import Main from "../components/Main.svelte";
+	import Header from "../components/Header.svelte";
+	import Step from "../components/Step.svelte";
+
+</script>
+
+<Main />
